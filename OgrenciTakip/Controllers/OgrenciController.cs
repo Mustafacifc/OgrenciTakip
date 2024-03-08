2cfe1319f7bba78d559ef7d39e7cf246ddf2a2f2
mustafa471 <mustafa471@BPC12>
2024-03-08 10:51:13 +0300

    Proje dosyası ekle.

---
+++ OgrenciTakip/Controllers/OgrenciController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OgrenciTakip.Models;
+using System.Security.Cryptography;
+
+namespace OgrenciTakip.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        static List<OgrenciModel> ogrenciler = new List<OgrenciModel>();
+
+        [HttpGet]
+        public IActionResult Kayit(OgrenciModel ogrenci)
+        {
+           ogrenciler.Add(ogrenci);
+            if (ogrenci.OkulNo < 1)
+            {
+                return Content("Öğrenci Numarası Geçerli Değil");
+            }
+            else if (string.IsNullOrEmpty(ogrenci.Isim))
+            {
+                return Content("Öğrenci Adi Geçerli Değil");
+            }
+            else if(string.IsNullOrEmpty(ogrenci.SoyIsim))
+            {
+                return Content("Öğrenci Soyadı Geçerli Değil");
+            }
+            else if(string.IsNullOrEmpty (ogrenci.Sinifi))
+            {
+                return Content("Sinifiniz Geçerli Değil");
+            }
+            else
+            {
+                return Content("Öğrenci Kayıdı Başarılıdır");
+            }
+        }
+        public IActionResult Listele(OgrenciModel ogrenci)
+        {
+            return View(ogrenciler);
+        }
+
+    }
+}
